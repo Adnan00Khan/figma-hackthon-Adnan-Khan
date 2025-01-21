12d4da75d0f05aab33ae07d15c3b8a3bf94e0c3d
M Adnan Khan <160040993+Adnan00Khan@users.noreply.github.com>
2025-01-22 04:29:02 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'; // Make sure React is imported
+import React from 'react'; 
 import CompanyLogo from './components/com-logo';
 import FeaturedProducts from './components/f-products';
 import Header from './components/header';
@@ -9,22 +9,22 @@ import TopCategories from './components/top-c';
 const Home: React.FC = () => {
   return (
     <div className="space-y-10">
-      {/* Header Component */}
+    
       <Header  />
 
-      {/* Company Logo */}
+     
       <CompanyLogo  />
 
-      {/* Featured Products */}
+    
       <FeaturedProducts  />
 
-      {/* Top Categories */}
+      
       <TopCategories  />
 
-      {/* Hot Category */}
+  
       <HotCategory  />
 
-      {/* Our Products */}
+    
       <OurProducts  />
     </div>
   );
